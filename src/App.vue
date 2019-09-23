e83d7a252d7fdf8d53b0e757cd9c602197ee0e3f
Aaron Ingram <aaron.ingram@pm.me>
2019-09-22 22:36:12 -0400

    Add bootstrap to application I started making custom styles for input fields and containers, but it's just easier to use Bootstrap. This will also make it easier to make the app responsive, since the electron window can be resized.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
   </div>
 </template>
 
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+@Component({})
+export default class App extends Vue { }
+</script>
+
 <style lang="scss">
 @import 'scss/main';
 </style>
